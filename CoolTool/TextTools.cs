1bad9cc948f5f380b08ba6e67ff231f05449daff
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2015-11-16 21:47:30 +0300

    Initial

---
+++ CoolTool/TextTools.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace CoolTool {
+	public static class TextTools {
+
+		private static string letters = " 1234567890abcdefghijklmnopqrstuvwxyz.,-=+!*$%#@абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+
+		public static string GetAeroText(string fromText, string toText, int currentTime, int totalTime){
+			string res = "";
+
+			if (currentTime < 0 || totalTime < 1)
+				throw new ArgumentOutOfRangeException("Need currentTime>=0, totalTime>=1");
+			if (currentTime > totalTime)
+				currentTime = totalTime;
+
+			fromText = fromText.PadRight(toText.Length);
+			toText = toText.PadRight(fromText.Length);
+			int deltaI = (int)(((double)currentTime / (double)totalTime) * 100);
+
+			string _fromText = fromText.ToLower();
+			string _toText = toText.ToLower();
+
+			for (int i = 0; i < toText.Length; i++) {
+
+				int startLetterPosition = letters.IndexOf(_fromText[i]);
+				int stopLetterPosition = letters.IndexOf(_toText[i]);
+				if (startLetterPosition == -1 || stopLetterPosition == -1) {
+					res += toText[i];
+					continue;
+				}
+
+				int deltaPosition = 0;
+				if (startLetterPosition < stopLetterPosition) {
+					deltaPosition = startLetterPosition + deltaI;
+					if (deltaPosition > stopLetterPosition)
+						deltaPosition = stopLetterPosition;
+				}else {
+					deltaPosition = startLetterPosition - deltaI;
+					if (deltaPosition < stopLetterPosition)
+						deltaPosition = stopLetterPosition;
+				}
+
+				string letter = letters[deltaPosition].ToString();
+				if (Char.IsLower(fromText[i]) && Char.IsUpper(fromText[i])) {
+					if (deltaI < 50)
+						res += letter.ToLower();
+					else
+						res += letter.ToUpper();
+				} else if (Char.IsUpper(fromText[i]) && Char.IsLower(fromText[i])) {
+					if (deltaI < 50)
+						res += letter.ToUpper();
+					else
+						res += letter.ToLower();
+				} else if (Char.IsUpper(fromText[i]) && Char.IsUpper(fromText[i])) {
+					res += letter.ToUpper();
+				} else 
+					res += letter;
+			}
+			return res;
+		}
+
+		public static string LiveLine(string text, int size, int currentTime, int totalTime){
+			string res = text.PadLeft(text.Length + size);
+			res = res.PadRight(res.Length + size);
+			int pos = (int)((res.Length-size) * ((double)currentTime / (double)totalTime));
+			return res.Substring(pos, size).PadRight(size);
+		}
+
+		public static string LiveToRight(string text, int currentTime, int totalTime){
+			int pos = (int)((text.Length) * ((double)currentTime / (double)totalTime));
+			return text.Substring(0, pos);
+		}
+
+		public static string LiveToLeft(string text, int currentTime, int totalTime){
+			int pos = (int)((text.Length) * ((double)currentTime / (double)totalTime));
+			return text.Substring(text.Length - pos, text.Length - (text.Length - pos)).PadLeft(text.Length);
+		}
+
+		public static string LiveFromCenter(string text, int currentTime, int totalTime){
+			if (currentTime == totalTime)
+				return text;
+			int pos = (int)((text.Length / 2) * ((double)currentTime / (double)totalTime));
+			Console.WriteLine("{0}  {1}  {2}", pos,text.Length / 2 - pos, pos * 2);
+			return text.Substring(text.Length / 2 - pos, pos * 2).PadLeft(text.Length / 2).PadRight(text.Length);
+		}
+	}
+
+}
+
---
+++ Test/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Test {
+	class MainClass {
+		public static void Main(string[] args) {
+			for (int d = 0; d <= 50; d += 1) {
+				Console.WriteLine("[{0}]", CoolTool.TextTools.LiveFromCenter("9876543210123456789", d, 50));
+			}
+		}
+	}
+}
